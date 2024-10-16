f4c8fbb69177cc60298851461d5fb4f4c046bbb0
SahilHadap <sahil.hadap1343@gmail.com>
2024-10-16 18:59:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "0ea2bdb2e0714ed0a010339f866ae4b0";
 const url = "https://newsapi.org/v2/everything?q=";
 
-window.addEventListener("load", () => fetchNews("India"));
-
-function reload() {
-    window.location.reload();
-}
+window.addEventListener("load", () => fetchNews("Technology"));
 
 async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
@@ -14,17 +10,18 @@ async function fetchNews(query) {
 }
 
 function bindData(articles) {
-    const cardsContainer = document.getElementById("cards-container");
+    const cardsContainer = document.getElementById("cardscontainer");
     const newsCardTemplate = document.getElementById("template-news-card");
 
     cardsContainer.innerHTML = "";
 
     articles.forEach((article) => {
         if (!article.urlToImage) return;
+
         const cardClone = newsCardTemplate.content.cloneNode(true);
         fillDataInCard(cardClone, article);
         cardsContainer.appendChild(cardClone);
-    });
+    })
 }
 
 function fillDataInCard(cardClone, article) {
@@ -34,18 +31,16 @@ function fillDataInCard(cardClone, article) {
     const newsDesc = cardClone.querySelector("#news-desc");
 
     newsImg.src = article.urlToImage;
-    newsTitle.innerHTML = article.title;
-    newsDesc.innerHTML = article.description;
+    newsTitle.innerHTML = `${article.title.slice(0, 60)}...`;
+    newsDesc.innerHTML = `${article.description.slice(0, 150)}...`;
 
-    const date = new Date(article.publishedAt).toLocaleString("en-US", {
-        timeZone: "Asia/Jakarta",
-    });
+    const date = new Date(article.publishedAt).toLocaleString("en-US", { timeZone: "Asia/Jakarta" })
 
     newsSource.innerHTML = `${article.source.name} · ${date}`;
 
     cardClone.firstElementChild.addEventListener("click", () => {
         window.open(article.url, "_blank");
-    });
+    })
 }
 
 let curSelectedNav = null;
@@ -66,4 +61,4 @@ searchButton.addEventListener("click", () => {
     fetchNews(query);
     curSelectedNav?.classList.remove("active");
     curSelectedNav = null;
-});
+})
