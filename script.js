989411e4faa427b0bb26dabba80a94e454540110
SahilHadap <sahil.hadap1343@gmail.com>
2024-10-16 19:26:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "0ea2bdb2e0714ed0a010339f866ae4b0";
+const API_KEY = "2fe6db26870b4f67bf63cbd54d03e340";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("Technology"));
